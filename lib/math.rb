b16a7e5352fe7c3b5839e9fc89d321d8bc19452b
Dante Spencer <spencerdantel@gmail.com>
2019-07-03 21:56:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,28 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+puts sum
+return sum
 end
 
 def subtraction(num1, num2)
-
+difference = num1 - num2
+puts difference
+return difference
 end
 
 def division(num1, num2)
-
+quotient = num1 / num2
+p quotient
 end
 
 def multiplication(num1, num2)
-
+product = num1 * num2
+p product
 end
 
 def modulo(num1, num2)
-
+quotient = num1 / num2
+p
 end
 
 def square_root(num)
